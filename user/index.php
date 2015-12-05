9a5a545860bd31db22f5c1ec4cfa0568c6028dc4
DevPGSV <DevPGSV@gmail.com>
2015-12-05 15:31:58 +0100

    Added "DarkHive" theme to user site

--- user/index.php
+++ user/index.php
@@ -6,10 +6,11 @@
       <!--<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
       <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
       <link rel="icon" href="favicon.ico" type="image/x-icon">
-      <link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+      
       <link rel="stylesheet" href="https://cdn.datatables.net/1.10.10/css/dataTables.jqueryui.min.css">
       <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-      <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+      <script src="http://files.devpgsv.com/libs/jquery-ui-1.11.4.custom-DarkHive/jquery-ui.min.js"></script>
+      <link rel="stylesheet" href="http://files.devpgsv.com/libs/jquery-ui-1.11.4.custom-DarkHive/jquery-ui.css">
       <script src="../js/datepicker-es.js"></script>
       <script src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
       <script src="https://cdn.datatables.net/1.10.10/js/dataTables.jqueryui.min.js"></script>
@@ -20,12 +21,30 @@
             /*http://www.myfreetextures.com/wp-content/uploads/2014/10/white-fur-texture.jpg*/
             background-color: #464646;
          }
+         
+         #loginbox {
+            margin-top: 150px;
+            margin-left: auto;
+            margin-right: auto;
+            width: 350px;
+            text-align: center;
+            border-radius: 20px;
+            background-color: rgba(255, 255, 255, 0.4);
+            padding:20px 40px 20px 40px;
+            -webkit-transition: background-color 1s, border-radius 1s;
+            transition: background-color 1s, border-radius 1s;
+         }
+         
+         #loginbox:hover {
+            background-color: rgba(255, 255, 255, 0.7);
+            border-radius: 40px;
+         }
       </style>
    </head>
 <body>
-   <div id="loginbox" style="margin-top: 150px;">
-      <fieldset style="margin-left: auto; margin-right: auto; width: 350px;text-align: center;border-radius: 20px">
-         <input type="text" class="custom-jui-textbox" name="username" id="loginform_username" placeholder="Username" title="Username">
+   <div id="loginbox">
+      <fieldset style="border: none;">
+         <input type="text" class="custom-jui-textbox" name="username" id="loginform_username" placeholder="Username" title="Username" autocomplete="off">
          <br><input type="password" class="custom-jui-textbox" name="password" id="loginform_password" placeholder="Password" title="Password">
          <br><input type="submit" class="custom-jui-button" value="Login" style="padding:20px 40px 20px 40px;">
       </fieldset>
